60fcfb38d1f6ba1e98e608661b167eb3551fab15
Ashley Dixon <ashleydixon2012@gmail.com>
2020-11-17 23:03:27 -0700

    Flask works for tobs

--- app.py
+++ app.py
@@ -40,16 +40,29 @@ def precipitation():
     session.close()
 
     return jsonify(precipitation_data)
-@app.route("/api/v1.0/tobs")
-def tobs():
-    print("Tobs page requested...")
-
-
-
 @app.route("/api/v1.0/stations")
 def stations():
     print("Stations page requested...")
+    session = Session(engine)
+    stations = session.query(Station.station, Measurement.tobs).\
+    group_by(Station.station).\
+    order_by(func.count(Station.station).desc()).all()
 
+    session.close()
+    
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("Tobs page requested...")
+    session = Session(engine)
+    highest_temp = session.query(Station.station, Measurement.date, Measurement.tobs).\
+    filter(Station.station == 'USC00519281').\
+    filter(Measurement.date >= '2016-08-23').all()
+
+    session.close()
+
+    return jsonify(highest_temp)
 
 
 if __name__ == "__main__":
